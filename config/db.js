cdf8796682a4c86a2474059ab368dba3e2970c3d
rozak dev <rozak@codepolitan.com>
2024-09-05 17:31:45 +0700

    fix : config

--- config/db.js
+++ config/db.js
@@ -2,7 +2,13 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('faker', 'root', '', {
+/*
+Silahkan ubah configurasi berikut ini
+database_name = samakan dengan nama database kamu
+username = samakan dengan username kamu
+password = jika ada password silahkan samakan, jika tidak biarkan saja kosong
+*/
+const sequelize = new Sequelize('database_name', 'username', 'password', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false // Ini untuk Menghilangkan log query SQL dari console
